85dc3386feb3825b38e163f09f3d4d42d5a995e2
sky2007j <sky1901j@gmail.com>
2025-11-10 18:50:42 +0530

    taking input from console

---
+++ console apps/basic/inputing.cs
@@ -0,0 +1,17 @@
+class c2
+{
+  public static void Main()
+  {
+    int a,b;
+    a=int.Parse(System.Console.ReadLine());
+    b=int.Parse(System.Console.ReadLine());
+    System.Console.WriteLine("{0} {1} {2}",a,b,a+b);
+  }
+}
+
+input:10+20
+output:30
+for taking input of int->  int.Parse(System.Console.ReadLine())
+for taking input of string-> System.Console.ReadLine()
+for taking input of double-> double.Parse(System.Console.ReadLine())
+  
