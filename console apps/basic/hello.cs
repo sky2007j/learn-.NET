91c81e2e74a46815efe1a31c6e054b5a4c2b7697
sky2007j <sky1901j@gmail.com>
2025-11-10 18:43:06 +0530

    basic hello program

---
+++ console apps/basic/hello.cs
@@ -0,0 +1,16 @@
+namespace nm1
+{
+  class c1
+  {
+    public static void Main()
+    {
+      System.Console.WriteLine("hello");
+    }
+  }
+}
+
+
+
+how to compile it 
+d:\->csc hello.cs
+d:\->prg1.exe
